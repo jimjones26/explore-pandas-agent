d5886a3576837b51f1de1efca85ef91ad29a5665
jjones <jimmy.jones@tutanota.com>
2023-11-28 20:58:21 -0700

    Add .env file with API keys and load them in pandas_agent.py

--- src/pandas_agent.py
+++ src/pandas_agent.py
@@ -1,13 +1,17 @@
 # ------------------------------------------------------------------
 # Import Libraries
 # ------------------------------------------------------------------
-from dotenv import load_dotenv, load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from langchain import OpenAI
 from langchain.agents import (
     load_tools,
     initialize_agent,
-    create_pandas_dataframe_agent,
     Tool,
     AgentType,
 )
 import pandas as pd
+
+# ------------------------------------------------------------------
+# Load the OpenAI and Google Serp API tokens from .env file
+# ------------------------------------------------------------------
+load_dotenv(find_dotenv())
